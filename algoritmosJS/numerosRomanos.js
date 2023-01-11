c08ac4e04ce566f63f9364c1533b8409acc98645
HigorChagas <higornchagas@gmail.com>
2023-01-11 17:20:34 -0300

    Arquivos de algoritmo

--- algoritmosJS/numerosRomanos.js
+++ algoritmosJS/numerosRomanos.js
@@ -1,15 +1,31 @@
- 
- 
- // 1. Quais são os dados de entrada necessários?
- // Uma cadeia de caracteres.
- // 2. O que devo fazer com estes dados?
- // Devo verificar se a string fornecida é um palíndromo
- // 3. Quais são as restrições deste problema?
- // Somente caracteres alfanuméricos são permitidos
- // 4. Qual é o resultado esperado?
- // Exibir true caso a string fornecida for um palíndromo.
- // 5. Qual é a sequência de passos a serem feitas para chegar ao resultado esperado?
- // Pegar a string
- // Remover todos os caracteres não alfanuméricos.
- // Checar se a string passada contem espaços e remover.
- // Transformar todos os caracteres na mesma captalização.
+const convertToRoman = (num) => {
+    const map = {
+        1: 'I',
+        4: 'IV',
+        5: 'V',
+        9: 'IX',
+        10: 'X',
+        40: 'XL',
+        50: 'L',
+        90: 'XC',
+        100: 'C',
+        400: 'CD',
+        500: 'D',
+        900: 'CM',
+        1000: 'M',
+      };
+
+      let romanized = '';
+      const decimalKeys = Object.keys(map).reverse();
+
+      decimalKeys.forEach(key => {
+        while(key <= num) {
+            romanized += map[key];
+            num -= key;
+        }
+      });
+
+    return romanized;
+}
+
+console.log(convertToRoman(5))
--- testes.js
+++ testes.js
@@ -1,37 +1,20 @@
-const palindrome = (str) => {
-   let strFiltered = str.replace(/[^A-Za-z0-9]/g, '').toLowerCase();
-   let strReversed = strFiltered.split('').reverse().join('').toLowerCase();
-
-   return strFiltered === strReversed ? true : false;
+const getYear = (years, searchYear) => {
+    years.find((year) => {
+        return year === searchYear;
+    });
 }
 
-console.log(palindrome('testando'));
-
-
+console.log(getYear([2021, 1995, 1786, 2002], 1995));
 // 1. Quais são os dados de entrada necessários?
-// Uma cadeia de caracteres.
-// 2. O que devo fazer com estes dados?
-// Devo verificar se a string fornecida é um palíndromo
-// 3. Quais são as restrições deste problema?
-// Somente caracteres alfanuméricos são permitidos
-// 4. Qual é o resultado esperado?
-// Exibir true caso a string fornecida for um palíndromo.
-// 5. Qual é a sequência de passos a serem feitas para chegar ao resultado esperado?
-// Pegar a string
-// Remover todos os caracteres não alfanuméricos.
-// Checar se a string passada contem espaços e remover.
-// Transformar todos os caracteres na mesma captalização.
-
-
-
-
-
-// const jadenCase = new toJadenCase();
-// console.log(jadenCase);
-// Múltiplique o número por oito se for par e por nove os outros.
-// 
-// String.prototype.toJadenCase = function() {
-    //         return this.split(' ').map(function(word) {
-        //             return word.charAt(0).toUpperCase() + word.slice(1);
-        //         }).join('');
-        // }
+ // Um número
+ // 2. O que devo fazer com estes dados?
+ // Verificar quantos litros de água Nathan bebe por hora.
+ // 3. Quais são as restrições deste problema?
+ // Somente números são aceitos como argumento
+ // 4. Qual é o resultado esperado?
+ //  Exibir a quantidade de água que Nathan bebeu de acordo com o horário fornecido
+ // 5. Qual é a sequência de passos a serem feitas para chegar ao resultado esperado?
+ // Pegar o número de horas
+ // Multiplicar a quantidade de água bebida pelo número de horas
+ // Arredondar o valor para baixo.
+ // 
